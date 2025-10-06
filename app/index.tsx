ea62a22a6c5ff73cc7efe15e7c6234d8205c770a
atharva-embeds <atharva@embedsqure.com>
2025-10-06 05:30:23 +0000

    test apk build

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import "../global.css"
+
+import Splash from "components/splash";
+
+const Root = () => {
+  return <Splash />;
+};
+
+export default Root;
---
+++ components/Heading.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native"
+
+export default function Heading() {
+  return (
+    <View>
+      <Text className="text-black text-lg font-bold" >Hello</Text>
+    </View>
+  )
+}
